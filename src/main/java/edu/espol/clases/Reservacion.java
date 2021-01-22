edeff6f9173edbe2438124f831fa16c7e3338961
Alexandra Imaicela <aeimaice@espol.edu.ec>
2021-01-21 20:13:30 -0500

    constructor de reservacion

--- src/main/java/edu/espol/clases/Reservacion.java
+++ src/main/java/edu/espol/clases/Reservacion.java
@@ -5,10 +5,84 @@
  */
 package edu.espol.clases;
 
+import java.time.LocalDate;
+
 /**
  *
- * @author lenovo
+ * @author Alexandra Imaicela
  */
 public class Reservacion {
+     private Habitacion habitacion;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
     
-}
+    private String nombre;
+    private String identificacion;
+    private String paisOrigen;
+
+    private String formaPago;
+    
+    public LocalDate getFechaInicio(){
+        return fechaInicio;
+    }
+    
+    public void setFechaInicio(LocalDate fechaInicio){
+        this.fechaInicio=fechaInicio;
+    }
+    
+    public LocalDate getFechaFin(){
+        return fechaFin;
+    }
+    
+    public void setFechaFin(LocalDate fechaFin){
+        this.fechaFin=fechaFin;
+    }
+    
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getPaisOrigen() {
+        return paisOrigen;
+    }
+
+    public void setPaisOrigen(String paisOrigen) {
+        this.paisOrigen = paisOrigen;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public Reservacion(Hotel hotel,Habitacion habitacion, LocalDate fechaInicio, LocalDate fechaFin){
+        this.habitacion = habitacion;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;        
+    }
+    
+    } 
+
