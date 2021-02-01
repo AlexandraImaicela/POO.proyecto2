098a0e30834f31148be6f02815862b069f01b01a
Alexandra Imaicela <aeimaice@espol.edu.ec>
2021-02-01 12:14:31 -0500

    ReservasPanel

---
+++ src/main/java/edu/espol/saves/reservasPanel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.espol.saves;
+
+import edu.espol.clases.Reservacion;
+import edu.espol.proyectopoo.PrimaryController;
+import edu.espol.proyectopoo.reservasBoxController;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+
+import javafx.scene.layout.VBox;
+
+public class reservasPanel implements Initializable {
+
+    @FXML
+    private VBox content;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    private void loadReservas() throws IOException {
+        for (Reservacion reserva : PrimaryController.primaryController.habSeleccionada.getReservas()) {
+            FXMLLoader reservasLoader = new FXMLLoader(getClass().getResource("hotelbox.fxml"));
+            Parent reservasPanel = reservasLoader.load();
+            reservasBoxController controller = (reservasBoxController) reservasLoader.getController();
+
+        }
+    }
+
+}
+
